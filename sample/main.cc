f8ab5944d983777ada7aadd30ef24b60b671da29
Kishor Prins <kprinssu@uwaterloo.ca>
2015-07-12 09:20:18 -0400

    Added a logging function for debugging

--- sample/main.cc
+++ sample/main.cc
@@ -3,6 +3,7 @@
 #include <psp2/moduleinfo.h>
 #include <psp2/kernel/processmgr.h>
 
+#include <cstdio>
 #include <vita2d.h>
 
 #include "../libvitamenu/menu_manager.h"
@@ -11,7 +12,13 @@
 
 PSP2_MODULE_INFO(0, 0, "touchvitalibsample");
 
+void log(std::string s)
+{
+	printf("%s%s", s.c_str(), "\n");
+}
+
 int main() {
+	log("Starting");
 	vita2d_init();
 
 	char menu_name[] = "Test";
@@ -29,19 +36,20 @@ int main() {
 	SceTouchData touch;
 
 	while(1) {
-		vita2d_clear_screen();
-
 		//read input
 		sceCtrlPeekBufferPositive(0, &pad, 1);
 		sceTouchPeek(0, &touch, 1);
 
+		vita2d_start_drawing();
+		vita2d_clear_screen();
 
 		manager->draw();
-
 
 		vita2d_end_drawing();
 		vita2d_swap_buffers();
 	}
+
+	log("Stopped.\n");
 
 	vita2d_fini();
 
